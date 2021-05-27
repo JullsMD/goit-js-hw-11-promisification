f50c8234e674aa1009aaa25bad9aad29b25a6d7a
Julia Melnik <julls.hm.md@gmail.com>
2021-05-27 15:11:52 +0300

    ---

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// console.log('task1');
+console.log('task1');
 import './src/js/task1'
-// console.log('task2');
+console.log('task2');
 import './src/js/task2';
-// console.log('task3');
+console.log('task3');
 import './src/js/task3';
